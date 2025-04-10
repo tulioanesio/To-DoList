180e1e29c9de17e632c6f78deb1ac7b89a684a68
Tulio <tulioluv@gmail.com>
2025-04-10 15:03:54 -0300

    feat: add tesk and local storage done

---
+++ index.js
@@ -0,0 +1,39 @@
+let tasks = [];
+
+window.onload = function () {
+  let savedTasks = localStorage.getItem("savedTasks");
+  savedTasks = JSON.parse(savedTasks);
+  let pMsgTask = document.getElementById("pMsgTask");
+  if (savedTasks) {
+    pMsgTask.innerHTML = "";
+
+    savedTasks.forEach((tasks) => {
+      pMsgTask.innerHTML += `<input type="checkbox"> ${tasks}<br>`;
+    });
+  } else {
+  }
+};
+
+function addTask() {
+  if (localStorage.savedTasks) {
+    tasks = JSON.parse(localStorage.getItem("savedTasks"));
+  }
+
+  let taskInput = document.getElementById("taskInput").value;
+  let pMsgTask = document.getElementById("pMsgTask");
+
+  tasks.push(taskInput);
+  console.log(tasks);
+  localStorage.savedTasks = JSON.stringify(tasks);
+
+  pMsgTask.innerHTML = "";
+
+  tasks.forEach((task) => {
+    pMsgTask.innerHTML += `<input type="checkbox" class=check> ${task}<br>`;
+  });
+
+  if(check.checked == true){
+    console.log("ola")
+  }
+
+}
