8ec1c8892a367038c40aa51a086ba8a254bbe637
Tulio <tulioluv@gmail.com>
2025-04-11 16:59:30 -0300

    feat: now the add button is inactive if there is no text in the input. accordions added.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 let tasks = [];
 
+document.getElementById("btn-Add").disabled = true;
+document.getElementById("btn-Add").style.cursor = "not-allowed";
+
+document
+  .getElementById("taskInput")
+  .addEventListener("input", function (event) {
+    var conteudo = document.getElementById("taskInput").value;
+
+    if (conteudo !== null && conteudo !== "") {
+      document.getElementById("btn-Add").disabled = false;
+      document.getElementById("btn-Add").style.cursor = "pointer";
+      document.getElementById("btn-Add").style.color = "#78BAFD";
+    } else {
+      document.getElementById("btn-Add").disabled = true;
+      document.getElementById("btn-Add").style.cursor = "not-allowed";
+      document.getElementById("btn-Add").style.color = "#605e5c";
+    }
+  });
+
 window.onload = function () {
   let savedTasks = localStorage.getItem("savedTasks");
   savedTasks = JSON.parse(savedTasks);
@@ -8,8 +27,9 @@ window.onload = function () {
     pMsgTask.innerHTML = "";
 
     savedTasks.forEach((tasks) => {
-      pMsgTask.innerHTML += `<input type="checkbox"> ${tasks}<br>`;
+      pMsgTask.innerHTML += `<input type="checkbox" class=check> ${tasks}<br>`;
     });
+    pMsgPending.textContent = `Pendentes(${savedTasks.length})`;
   } else {
   }
 };
@@ -28,11 +48,31 @@ function addTask() {
 
   pMsgTask.innerHTML = "";
 
+  document.getElementById("btn-Add").disabled = false;
+  document.getElementById("btn-Add").style.cursor = "pointer";
   tasks.forEach((task) => {
     pMsgTask.innerHTML += `<input type="checkbox" class=check> ${task}<br>`;
   });
 
-  if(check.checked == true){
-  }
+  pMsgPending.textContent = `Pendentes(${tasks.length})`;
 
+  if (check.checked == true) {
+    pDoneTask.innerHTML += `<input type="checkbox" class=check> ${task}<br>`;
+  }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
